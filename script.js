9934079f8de9a6e61786ae869674957ac2ae5876
Vincent Baquiro <vincent.baquiro@dynatacorp.com>
2023-03-14 06:41:46 -0400

    update codes

--- script.js
+++ script.js
@@ -1,21 +1,53 @@
 const loginBtn = document.getElementById("login-btn");
-const getId = document.querySelectorAll("#btn");
-const getRem = document.querySelectorAll("#to-remove");
+const cardState = document.querySelectorAll("#card-state");
+const cardText = document.querySelectorAll(".card-text");
+const searchInput = document.getElementById("searchInput");
+const cardBtnDelete = document.querySelectorAll("#btn-del");
+const check = document.querySelectorAll("#check");
+const favorite = document.querySelectorAll("#favorite");
 
-//set active state
+//set active state in login btn
 loginBtn.onclick = () => {
   console.log(true);
   loginBtn.classList.toggle("active");
 };
 
-//removing class items
-for (let i = 0; i < getId.length; i++) {
-  for (let j = i; j < getRem.length; j++) {
-    console.log(getRem[j]);
-    getId[i].onclick = () => {
-      console.log(getRem[j].innerText);
-      getRem[j].remove();
+//removing news items
+cardBtnDelete.forEach((btn) => {
+  btn.onclick = () => {
+    const card = btn.closest("#card-state");
+    card.remove();
+  };
+});
+
+//search news
+
+searchInput.addEventListener("keyup", function () {
+  const inputQuery = this.value.toLowerCase();
+
+  for (let i = 0; i < cardState.length; i++) {
+    for (let j = i; j < cardText.length; i++) {
+      const itemsText = cardText[i].textContent.toLowerCase();
+
+      if (itemsText.includes(inputQuery)) {
+        cardState[i].style.display = "block";
+      } else {
+        cardState[i].style.display = "none";
+      }
+      break;
+    }
+  }
+});
+
+//usable state btn trigger
+
+function useFavorite(enableState, setColor) {
+  for (let i = 0; i < enableState.length; i++) {
+    enableState[i].onclick = () => {
+      enableState[i].classList.toggle(setColor);
     };
-    break;
   }
 }
+
+useFavorite(check, "blue");
+useFavorite(favorite, "blue");
